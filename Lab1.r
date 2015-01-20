c6ce0c9767e1bad2ba7e3ba6a10a77d2db564531
Tonatiuth M <tonatiuth451@gmail.com>
2015-01-19 20:19:31 -0600

    comentario

---
+++ Lab1.r
@@ -0,0 +1,14 @@
+lista<-list(uno=1:900,dos=50:500,tres=800:100)
+lista[[c(1,799)]]
+lista[[2]][[101]]
+lista[[1]][[869]]
+m<-matrix(1:1000,nrow=100,ncol=100)
+m[,98]
+m[59,]
+
+m[99,1]
+data<-bzfile("cbp12co.zip")
+data<-bzfile("cbp12co.zip")
+x<-readLines(data,4)
+con<-url("http://rt.com")
+y<-readLines(con,10)
